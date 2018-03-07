a2d7b776a647bf20f99991e1f0392dd6951c862c
changlingfei <1510243345@vip.henu.edu.cn>
2018-03-07 22:16:35 +0800

    push

--- App.js
+++ App.js
@@ -9,8 +9,23 @@ import {
   StyleSheet,
 } from 'react-native';
 import Pages from './src/Pages';
+import codePush from 'react-native-code-push';
 type Props = {};
 export default class App extends Component<Props> {
+    componentDidMount(){
+        codePush.sync({
+            updateDialog: {
+                appendReleaseDescription: true,
+                descriptionPrefix:'\n\n更新内容：\n',
+                title:'更新',
+                mandatoryUpdateMessage:'',
+                mandatoryContinueButtonLabel:'更新',
+            },
+            mandatoryInstallMode:codePush.InstallMode.IMMEDIATE,
+            //deploymentKey: '',
+        });
+        //codePush.sync();
+    }
   render() {
     return (
       <Pages/>
